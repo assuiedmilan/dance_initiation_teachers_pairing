a62072a6f73fc216bd41b7962d9f51d576f6db9e
Milan Assuied <milan.assuied@gmail.com>
2023-08-11 16:35:05 -0400

    Initialize the project

---
+++ Python/src/entry_points/teachers_pairing.py
@@ -0,0 +1,10 @@
+"""This module contains the main entry point of the software"""
+
+
+def run():
+    """Entry point"""
+    print("teachers_pairing main function called")
+
+
+if __name__ == '__main__':
+    run()
